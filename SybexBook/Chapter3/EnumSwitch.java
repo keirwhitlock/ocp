733f2407807ae2d0016f297a6e0cfcd4322f2f0c
Keir Whitlock <git@keirwhitlock.co.uk>
2025-02-17 09:44:57 +0000

    looking at switch expressions and enums, and their need to have all possible values covered, or have a default

---
+++ SybexBook/Chapter3/EnumSwitch.java
@@ -0,0 +1,19 @@
+public class EnumSwitch {
+
+    enum Season { SPRING, SUMMER, FALL, WINTER } // optional ; ?
+
+    public static void main(String[] args){
+
+        Season v = Season.SPRING;
+
+        String n = switch(v) {
+            case SPRING -> "Spring";
+            case SUMMER -> "Summer";
+            //case FALL -> "Fall";  // all values must be caught or add a default error: the switch expression does not cover all possible input values
+            case WINTER -> "Winter";
+            default -> "No Season"; // default fixes Switch Expression's need to have all bases covered if they arn't
+        }; // req ; for expressions
+
+        System.out.println(n);
+    }
+}
